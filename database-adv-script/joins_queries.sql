1e5d5555df600e6031a43cf7c953dbcf4cde16de
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-04 17:59:03 +0200

    Add ORDER BY clauses to all SELECT statements in joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,9 @@ SELECT
   users.email
 FROM
   bookings
-INNER JOIN users ON bookings.user_id = users.id;
+INNER JOIN users ON bookings.user_id = users.id
+ORDER BY
+  bookings.start_date;
 
 SELECT
   properties.id AS property_id,
@@ -18,7 +20,10 @@ SELECT
   reviews.comment
 FROM
   properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY
+  properties.id;
+
 
 SELECT
   users.id AS user_id,
@@ -29,4 +34,7 @@ SELECT
   bookings.end_date
 FROM
   users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+FULL OUTER JOIN bookings ON users.id = bookings.user_id
+ORDER BY
+  user_id,
+  booking_id;
