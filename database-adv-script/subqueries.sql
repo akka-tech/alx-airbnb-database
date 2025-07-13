08191fc8ffc728e6bc3e92a0f0c4e9b752654bda
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-13 23:59:21 +0200

    count

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,10 +1,20 @@
+
 SELECT *
 FROM Property
 WHERE property_id IN (
     SELECT property_id
     FROM Review
-    WHERE rating > 4.0
+    WHERE rating > 0  -- needed to use WHERE
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
 );
+
+
+SELECT *
+FROM User u
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
 
