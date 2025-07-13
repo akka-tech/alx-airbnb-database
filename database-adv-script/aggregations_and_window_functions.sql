c212d20a9b871158a8c9c5b140dffd7a038b650f
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-14 00:03:45 +0200

    Use SQL aggregation and window functions to analyze data.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,17 +1,10 @@
 SELECT
-  u.user_id,
-  u.first_name,
-  u.last_name,
-  COUNT(b.booking_id) AS total_bookings
-FROM User u
-JOIN Booking b ON u.user_id = b.user_id
-GROUP BY u.user_id, u.first_name, u.last_name;
-SELECT
   p.property_id,
-  p.name,
-  AVG(r.rating) AS average_rating
+  p.name AS property_name,
+  COUNT(b.booking_id) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
+  ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
 FROM Property p
-JOIN Review r ON p.property_id = r.property_id
-WHERE r.rating > 0
-GROUP BY p.property_id, p.name
-HAVING AVG(r.rating) > 4.0;
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name;
+
