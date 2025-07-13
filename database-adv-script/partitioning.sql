94eb2b0a10a4b3339df5e03096e23710ae6bf50c
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-14 00:09:57 +0200

    Implement table partitioning to optimize queries on large datasets.

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS Booking;
+
+CREATE TABLE Booking (
+  booking_id CHAR(36) PRIMARY KEY,
+  property_id CHAR(36),
+  user_id CHAR(36),
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  total_price DECIMAL NOT NULL,
+  status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  INDEX idx_start_date (start_date)
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+  PARTITION p2020 VALUES LESS THAN (2021),
+  PARTITION p2021 VALUES LESS THAN (2022),
+  PARTITION p2022 VALUES LESS THAN (2023),
+  PARTITION p2023 VALUES LESS THAN (2024),
+  PARTITION p2024 VALUES LESS THAN (2025),
+  PARTITION pmax VALUES LESS THAN MAXVALUE
+);
